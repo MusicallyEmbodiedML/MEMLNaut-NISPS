c633b9c2abab0ab3fe85d20bc52827687ae70ce1
chriskiefer <chrisk13fer@gmail.com>
2026-03-29 19:53:09 +0100

    PAF

--- MEMLNaut-NISPS.ino
+++ MEMLNaut-NISPS.ino
@@ -36,11 +36,11 @@
 
 // #define MEMLNAUT_MODE_TYPE MEMLNautModeSoundAnalysisMIDI
 // #define MEMLNAUT_MODE_TYPE MEMLNautModeXIASRI
-#define MEMLNAUT_MODE_TYPE MEMLNautModeVerbFX
+// #define MEMLNAUT_MODE_TYPE MEMLNautModeVerbFX
 // #define MEMLNAUT_MODE_TYPE MEMLNautModeBreakOr
 // #define MEMLNAUT_MODE_TYPE MEMLNautModeElysiamorfs
 // #define MEMLNAUT_MODE_TYPE MEMLNautModeChannelStrip
-// #define MEMLNAUT_MODE_TYPE MEMLNautModePAFSynth
+#define MEMLNAUT_MODE_TYPE MEMLNautModePAFSynth
 
 MEMLNAUT_MODE_TYPE AUDIO_MEM MEMLNautModeHub;
 
